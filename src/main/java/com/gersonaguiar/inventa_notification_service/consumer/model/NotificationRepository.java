44a26103ea1f8d141f20478feb059d69d965f766
ggersonmg <gersoncafilho@gmail.com>
2025-02-02 18:34:40 -0300

    Remove `NotificationRepository` and configure PostgreSQL integration
    Deleted the unused `NotificationRepository` and adjusted `NotificationLog` to streamline the model. Added PostgreSQL dependencies, connection settings, and Hibernate dialect in `application.properties` and `pom.xml` to support database integration.

--- src/main/java/com/gersonaguiar/inventa_notification_service/consumer/model/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gersonaguiar.inventa_notification_service.consumer.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<NotificationLog, Long> {}
